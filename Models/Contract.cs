c96dfd4b7e01f2e22d9b536dd25646683c560554
David Marek <davidmarek14@gmail.com>
2025-06-08 16:50:14 +0200

    Add ShortId property to Contract model and update views for better ID display

--- Models/Contract.cs
+++ Models/Contract.cs
@@ -5,9 +5,8 @@ namespace BlogicCRM.Models;
 public class Contract
 {
     [Key] public Guid Id { get; set; }
-    
-    [DataType(DataType.Date)]
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    [DataType(DataType.Date)] public DateTime CreatedAt { get; set; } = DateTime.Now;
 
     [Required]
     [DataType(DataType.Date)]
@@ -23,15 +22,20 @@ public class Contract
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
     public DateTime Closed { get; set; }
-    
+
+    // Foreign keys
     public Guid ClientId { get; set; }
     public Guid AdminId { get; set; }
     public int InstitutionId { get; set; }
 
-    
+    // Navigation properties
     public Client Client { get; set; } = null!;
     public Consultant Admin { get; set; } = null!;
     public Institution Institution { get; set; } = null!;
     public ICollection<Consultant> Consultants { get; set; } = [];
-    
+
+    // Computed properties
+
+    [Display(Name = "ID")]
+    public string ShortId => Id.ToString()[..4] + "..." + Id.ToString()[(Id.ToString().Length - 4)..];
 }
